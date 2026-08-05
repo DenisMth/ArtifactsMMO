1e187212b83cbbe2ab853001333af25d511006c2
Denis Mathieu <d.mathieu@stag.technofuturtic.education>
2026-08-05 12:44:35 +0200

    favico path

--- api.py
+++ api.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from Schemas.Command import Command
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 
 def create_app(controller):
     app = FastAPI()
+
+    @app.get("/favicon.ico", include_in_schema=False)
+    async def favicon():
+        return FileResponse(BASE_DIR / "favicon.ico")
 
     @app.get("/")
     async def root():
